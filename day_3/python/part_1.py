e6b2f8ed62a1eb2d93909932c0b597b010338766
Daniel Martinez <danmartinez78@gmail.com>
2021-12-03 17:09:56 -0600

    rewrite of day 3 part 1 to use numpy

--- day_3/python/part_1.py
+++ day_3/python/part_1.py
@@ -1,10 +1,28 @@
 import sys, getopt
 import numpy as np
+from math import ceil
+import copy
 
 def read_input(fn):
     with open(fn) as f:
         lines = f.read().splitlines()
+        lines = [list(line) for line in lines]
+        for i in range(len(lines)):
+          lines[i] = list(map(int, lines[i]))
     return lines
+
+def find_most_least_common(data):
+  gamma = ''
+  epsilon = ''
+  data = np.asarray(data)
+  count = np.sum(data, axis = 0)
+  result = count >= ceil(data.shape[0]/2)
+  gamma = [int(x) for x in result]
+  epsilon = [int(x) for x in [not elem for elem in result]]
+  gamma = [str(intgr) for intgr in gamma]
+  epsilon = [str(intgr) for intgr in epsilon]
+  return ''.join(gamma), ''.join(epsilon)
+
 
 def main(argv):
     try:
@@ -20,24 +38,7 @@ def main(argv):
          inputfile = arg
     
     data = read_input("../inputs/" + inputfile)
-    print(data)
-    data = np.asarray(data)
-    print(data)
-    gamma = ''
-    epsilon = ''
-    counts = np.zeros(len(data[0]))
-    for bits in data:
-      print(bits)
-      for i in range(len(data[0])):
-        if bits[i] == '1':
-          counts[i] += 1
-    for i in range(len(counts)):
-      if counts[i] > int(len(data)/2):
-        gamma +=  '1'
-        epsilon += '0'
-      else:
-        gamma +=  '0'
-        epsilon += '1'
+    gamma, epsilon = find_most_least_common(copy.copy(data))
     print(gamma, epsilon)
     print(int(gamma, 2) * int(epsilon, 2))
 
