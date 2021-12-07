59105f100527b30c2ff6949b50c2d2d884bdc061
Daniel Martinez <danmartinez78@gmail.com>
2021-12-07 09:22:35 -0600

    finished day_7 python implementation

---
+++ day_7/python/part_1.py
@@ -0,0 +1,39 @@
+import sys, getopt
+import numpy as np
+from math import ceil
+import copy
+
+def read_input(fn):
+    with open(fn) as f:
+        lines = f.read().split('\n')
+        lines = [l.replace(" ", "") for l in lines]
+        lines = [int(n) for n in lines[0].split(',')]
+    return lines
+
+def main(argv):
+    try:
+      opts, args = getopt.getopt(argv,"hi:",["ifile="])
+    except getopt.GetoptError:
+      print('part_1.py -i <inputfile>')
+      sys.exit(2)
+    for opt, arg in opts:
+      if opt == '-h':
+         print('part_1.py -i <inputfile>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+    
+    data = read_input("../inputs/" + inputfile)
+    data = np.asarray(data)
+    pos = []
+    costs = []
+    for i in range(np.min(data), np.max(data)):
+      pos.append(i)
+      costs.append(np.sum(abs(data-i)))
+    min_cost = min(costs)
+    loc = np.where(costs == min_cost)[0][0]
+    print(pos[loc], min_cost)
+    
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
